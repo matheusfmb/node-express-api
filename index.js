192bc96a34bc10db8522a1c0102713b616f6e349
Matheus Felipe <matheusbarros9107@gmail.com>
2023-12-29 19:47:09 -0300

    Connecting  database
    Connecting  database mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const dotenv = require('dotenv');
 const express = require("express");
+const connectToDB = require("./src/database/connect");
 const port = 8080;
 const app = express();
 
+dotenv.config()
+connectToDB()
+
 const userRoutes = require("./src/routes/userRoutes");
 app.use("/api", userRoutes); 
+
+
 app.listen(port, () => console.log(`Server Rodando na porta ${port}!`));
---
+++ src/database/connect.js
@@ -0,0 +1,19 @@
+// connectToDB.js
+const mongoose = require("mongoose");
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.de3aqwr.mongodb.net/?retryWrites=true&w=majority`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        );
+        console.log("Conexão Realizada com sucesso!");
+    } catch (error) {
+        console.error("Erro ao conectar ao banco de dados:", error.message);
+    }
+};
+
+module.exports = connectToDB;
