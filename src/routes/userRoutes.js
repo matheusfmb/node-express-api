3c8fd263c587b5ebe3f431455eec36cbde69c589
Matheus Felipe <matheusbarros9107@gmail.com>
2023-12-29 20:54:13 -0300

    Create method to get user by id

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,20 +5,34 @@ const router = express.Router();
 router.post("/users", async (req, res) => {
     try {
         const user = await UserModel.create(req.body)
-        res.status(201).send(user)
+        return res.status(201).send(user)
     }catch (error){
-        res.status(500).send(error.message);
+        return res.status(500).send(error.message);
     }
 });
 
 router.get("/users", async (req, res) =>{
     try{
         const users = await UserModel.find({});
-        res.status(200).send(users)
+        return res.status(200).send(users)
     }catch (error){
-        res.status(500).send(error.message)
+        return res.status(500).send(error.message)
     }
 });
 
+router.get('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+
+        if (!user) {
+            return res.status(404).send("Usuário não encontrado");
+        }
+
+        return res.status(200).send(user);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
 
 module.exports = router;
